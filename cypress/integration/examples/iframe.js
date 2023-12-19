79c1c1baf6740cc9d667fc5b88a9ea3d871115df
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-12-19 11:48:24 +1100

    cypress framework

---
+++ cypress/integration/examples/iframe.js
@@ -0,0 +1,21 @@
+/// <reference types = 'Cypress' />
+/// <reference types = 'cypress-iframe' />
+
+import 'cypress-iframe'
+
+describe('iframe handling test', function()
+{
+    it('should handle data inside frames', function() {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.frameLoaded('#courses-iframe')
+
+        cy.iframe().find("a[href*='mentorship']").eq(0).click()
+
+        cy.iframe().find("h1[class*='pricing-title']").should('have.length','2')
+
+        
+        
+
+    })
+})
